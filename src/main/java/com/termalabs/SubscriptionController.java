bae3fd09a98c575f6158a48b3e04d3033bcee02e
Dan Schaffer <dan.schaffer@termalabs.com>
2017-09-06 15:09:06 -0600

    temp commit

---
+++ src/main/java/com/termalabs/SubscriptionController.java
@@ -0,0 +1,14 @@
+package com.termalabs;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubscriptionController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
