6a41e5c9b16ea0df97640222b56e4e5feebd41f2
Dan Schaffer <dan.schaffer@termalabs.com>
2017-09-07 09:27:13 -0600

    temp commit

--- src/main/java/com/termalabs/dummy_jaws/DummyJaws.java
+++ src/main/java/com/termalabs/dummy_jaws/DummyJaws.java
@@ -11,25 +11,22 @@ public class DummyJaws {
 	final static String PREDICTION_SUBSCRIPTION_QUEUE = "predictionSubscriptionQueue";
 
 	public static void main(String[] args) throws Exception {
-	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("localhost");
-	    Connection connection = factory.newConnection();
-	    Channel channel = connection.createChannel();
-	    
-        AMQP.BasicProperties queueProperties = new AMQP.BasicProperties.Builder()
-                .contentType("text/plain")
-                .deliveryMode(2)
-                .build();
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try (Connection connection = factory.newConnection()) {
 
+			Channel channel = connection.createChannel();
 
-	    channel.queueDeclare(PREDICTION_SUBSCRIPTION_QUEUE, true, false, false, null);
-	    String message = new Prediction("some prediction").asJson();
-	    channel.basicPublish("", PREDICTION_SUBSCRIPTION_QUEUE, queueProperties, message.getBytes("UTF-8"));
-	    System.out.println(" [x] Sent '" + message + "'");
+			AMQP.BasicProperties queueProperties = new AMQP.BasicProperties.Builder().contentType("text/plain")
+					.deliveryMode(2).build();
 
-	    channel.close();
-	    connection.close();
+			channel.queueDeclare(PREDICTION_SUBSCRIPTION_QUEUE, true, false, false, null);
+			String message = new Prediction("some prediction").asJson();
+			channel.basicPublish("", PREDICTION_SUBSCRIPTION_QUEUE, queueProperties, message.getBytes("UTF-8"));
+			System.out.println(" [x] Sent '" + message + "'");
 
+			channel.close();
+		}
 
 	}
 
