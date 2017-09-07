063df8be10bf8db1df296e3aeb83d9f3b9bbcca2
Dan Schaffer <dan.schaffer@termalabs.com>
2017-09-07 09:21:29 -0600

    Now accepting predictions from dummy jaws

---
+++ src/main/java/com/termalabs/dummy_jaws/DummyJaws.java
@@ -0,0 +1,36 @@
+package com.termalabs.dummy_jaws;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import com.termalabs.subscriptions.Prediction;
+
+public class DummyJaws {
+	final static String PREDICTION_SUBSCRIPTION_QUEUE = "predictionSubscriptionQueue";
+
+	public static void main(String[] args) throws Exception {
+	    ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+	    
+        AMQP.BasicProperties queueProperties = new AMQP.BasicProperties.Builder()
+                .contentType("text/plain")
+                .deliveryMode(2)
+                .build();
+
+
+	    channel.queueDeclare(PREDICTION_SUBSCRIPTION_QUEUE, true, false, false, null);
+	    String message = new Prediction("some prediction").asJson();
+	    channel.basicPublish("", PREDICTION_SUBSCRIPTION_QUEUE, queueProperties, message.getBytes("UTF-8"));
+	    System.out.println(" [x] Sent '" + message + "'");
+
+	    channel.close();
+	    connection.close();
+
+
+	}
+
+}
--- src/main/java/com/termalabs/subscriptions/Prediction.java
+++ src/main/java/com/termalabs/subscriptions/Prediction.java
@@ -2,11 +2,15 @@ package com.termalabs.subscriptions;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
-public class Prediction {
+public class Prediction extends SubscriptionContent {
 	private String content;
-
+	
 	public Prediction() {
 		
+	}
+
+	public Prediction(String content) {
+		this.content = content;
 	}
 
 	public String getContent() {
