9c0348035c1b64ad352098e456090f90a8b4790d
Dan Schaffer <dan.schaffer@termalabs.com>
2017-09-07 09:02:22 -0600

    Now dumped the Add/Delete Q stuff

--- src/main/java/com/termalabs/subscriptions/SubscriptionConstants.java
+++
@@ -1,7 +0,0 @@
-package com.termalabs.subscriptions;
-
-public class SubscriptionConstants {
-	final static String ADD_SUBSCRIPTION_QUEUE = "addSubscriptionQueue";
-	final static String DELETE_SUBSCRIPTION_QUEUE = "deleteSubscriptionQueue";
-	final static String PREDICTION_SUBSCRIPTION_QUEUE = "predictionSubscriptionQueue";
-}
--- src/main/java/com/termalabs/subscriptions/SubscriptionService.java
+++ src/main/java/com/termalabs/subscriptions/SubscriptionService.java
@@ -1,6 +1,12 @@
 package com.termalabs.subscriptions;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.PreDestroy;
 
@@ -8,66 +14,73 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 
-import static com.termalabs.subscriptions.SubscriptionConstants.ADD_SUBSCRIPTION_QUEUE;
-import static com.termalabs.subscriptions.SubscriptionConstants.DELETE_SUBSCRIPTION_QUEUE;
+import com.termalabs.subscriptions.Subscription.SubscriptionType;
 
 @Component
 public class SubscriptionService {
 	
+	final static String PREDICTION_SUBSCRIPTION_QUEUE = "predictionSubscriptionQueue";
+
 	private final ObjectMapper om;
-	private final Channel addChannel;
-	private final Channel deleteChannel;
-	private final BasicProperties queueProperties;
 	private final Connection connection;
+	private final Map<SubscriptionType, Set<URL>> subscriptions;
+	private Channel subscriptionChannel;
 
 	public SubscriptionService() throws Exception {
 
 		this.om = new ObjectMapper();
-		queueProperties = new AMQP.BasicProperties.Builder()
-				.contentType("text/plain")
-				.deliveryMode(2)
-				.build();
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		this.connection = factory.newConnection();
-		
-		addChannel = buildChannel(ADD_SUBSCRIPTION_QUEUE);
-		deleteChannel = buildChannel(DELETE_SUBSCRIPTION_QUEUE);
+		this.subscriptions = new HashMap<>();
+		Arrays.asList(SubscriptionType.values()).forEach(e -> subscriptions.put(e, new HashSet<>()));
+		listenForPredictions();
 
 	}
 	
 	public void addSubscription(String url, String type) throws Exception {
 		Subscription subscription = new Subscription(url, type);
-		String asJson = om.writeValueAsString(subscription);
-		addChannel.basicPublish("", ADD_SUBSCRIPTION_QUEUE, queueProperties, asJson.getBytes("UTF-8"));
+		System.out.println(" Adding subscription " + subscription);
+		subscriptions.get(subscription.getType()).add(new URL("https://" + subscription.getUrl()));
 
 	}
 
 	public void deleteSubscription(String url, String type) throws Exception {
 		Subscription subscription = new Subscription(url, type);
-		String asJson = om.writeValueAsString(subscription);
-		deleteChannel.basicPublish("", DELETE_SUBSCRIPTION_QUEUE, queueProperties, asJson.getBytes("UTF-8"));
+		System.out.println(" Deleting subscription " + subscription);
+		subscriptions.get(subscription.getType()).remove(new URL("https://" + subscription.getUrl()));
 		
 	}
 
     @PreDestroy
     public void close() throws Exception {
-		addChannel.close();
-		deleteChannel.close();
+		subscriptionChannel.close();
 		connection.close();
     }
-    
-	private Channel buildChannel(String queueName) throws IOException {
-		Channel result = connection.createChannel();
-		result.queueDeclare(queueName, true, false, false, null);
-		return result;
-	}
 
-	
+	private void listenForPredictions() throws Exception {
+		this.subscriptionChannel = connection.createChannel();
+
+		subscriptionChannel.queueDeclare(PREDICTION_SUBSCRIPTION_QUEUE, true, false, false, null);
+
+		Consumer subscriptionConsumer = new DefaultConsumer(subscriptionChannel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+					byte[] body) throws IOException {
+				Prediction prediction = om.readValue(body, Prediction.class);
+				subscriptions.get(Subscription.SubscriptionType.PREDICTION)
+				    .forEach(url->System.out.println("Sending prediction to URL " + prediction +  " " + url));
+			}
+		};
+		subscriptionChannel.basicConsume(PREDICTION_SUBSCRIPTION_QUEUE, true, subscriptionConsumer);
+		
+	}
 
 }
