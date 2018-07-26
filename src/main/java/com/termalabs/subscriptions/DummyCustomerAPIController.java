d5db29e2f6bdc6186ab1dbc2e0d2309d6568710d
Dan Schaffer <dan.schaffer@termalabs.com>
2018-07-26 07:32:31 -0600

    Some cleanup

--- src/main/java/com/termalabs/subscriptions/DummyCustomerAPIController.java
+++ src/main/java/com/termalabs/subscriptions/DummyCustomerAPIController.java
@@ -1,9 +1,11 @@
 package com.termalabs.subscriptions;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,40 +17,62 @@ public class DummyCustomerAPIController {
 
 	private static Integer msgCount = 0;
 
-	@RequestMapping(
-			method = RequestMethod.PUT, value = "alert",
+	@PutMapping(
+			value = "alert",
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void alert(@RequestBody String alert) throws Exception {
 		System.out.println(String.format("Received alert %s", alert));
 		delayIfConfigured("ALERT_DELAY_IN_SECONDS");
 	}
 
-	@RequestMapping(
-			method = RequestMethod.PUT, value = "prediction",
+	@PutMapping(
+			value = "prediction/{jobStreamId}",
 			consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void prediction(@RequestBody String prediction) throws Exception {
-		System.out.println(String.format("Received prediction %s", prediction));
+	public void predictionViaPut(
+			@PathVariable String jobStreamId,
+			@RequestBody String prediction) throws Exception {
+		System.out.println(String.format("For jobStreamId %s, Received prediction %s via PUT", 
+				jobStreamId, prediction));
+		delayIfConfigured("PREDICTION_DELAY_IN_SECONDS");
+	}
+	
+	@PostMapping(
+			value = "prediction/{jobStreamId}",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void predictionViaPost(
+			@PathVariable String jobStreamId,
+			@RequestBody String prediction) throws Exception {
+		System.out.println(String.format("For jobStreamId %s, Received prediction %s via POST", 
+				jobStreamId, prediction));
 		delayIfConfigured("PREDICTION_DELAY_IN_SECONDS");
 	}
 
-	@RequestMapping(
-			method = RequestMethod.PUT, value = "schedulerData",
+	@PutMapping(
+			value = "schedulerData",
 			consumes = MediaType.APPLICATION_JSON_VALUE
 			)
 	public void schedulerData(@RequestBody String schedulerData) throws Exception {
 		synchronized (msgCount) {
 			System.out.println(String.format("Received scheduler data for msg %d", msgCount));
-			msgCount += 1;
+			msgCount = msgCount + 1;
 		}
 		delayIfConfigured("SCHEDULER_DELAY_IN_SECONDS");
 	}
 
-	@RequestMapping(
-			method = RequestMethod.PUT, value = "event",
+	@PutMapping(
+			value = "event",
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void event(@RequestBody String event) throws Exception {
 		System.out.println(String.format("Received event %s", event));
 		delayIfConfigured("EVENT_DELAY_IN_SECONDS");
+	}
+	
+	@PutMapping(
+			value = "jobStreamRunTransitioned",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void jobStreamRunDeleted(@RequestBody String jobStreamRunTransitioned) throws Exception {
+		System.out.println(String.format("Received jobStreamRunTransitioned %s", jobStreamRunTransitioned));
+		delayIfConfigured("JOB_STREAM_RUN_TRANSITIONED_DELAY_IN_SECONDS");
 	}
 
 	private void delayIfConfigured(String delayEnvVariable) throws InterruptedException {
